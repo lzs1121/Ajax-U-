744552060e2e24223e2542320d343b78b4f059ef
Sean Li <lizishuo1121@gmail.com>
2019-08-21 22:11:28 +1000

    make axios file

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import registerServiceWorker from './registerServiceWorker';
 // use axios default to set the baseURL, headers.common, headers.post;
 //  set he interceptor request and response with cb request and error
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(request => {
